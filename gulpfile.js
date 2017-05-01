a0e71cf689a29eeba37bc3f3d8caafe19959ff4e
ldubos <ldubos@student.42.fr>
2017-05-01 21:51:50 +0200

    Change indentation style (i think is a bad idea but :sweat_smile:)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var browsersync = require('browser-sync').create(),
-    gulp        = require('gulp'),
-    autoprefixer= require('gulp-autoprefixer'),
-    cleancss    = require('gulp-clean-css'),
-    concat      = require('gulp-concat'),
-    csscomb     = require('gulp-csscomb'),
-    imagemin    = require('gulp-imagemin'),
-    csslint     = require('gulp-csslint'),
-    jshint      = require('gulp-jshint'),
-    rename      = require('gulp-rename'),
-    sass        = require('gulp-sass'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    uglify      = require('gulp-uglify');
+var browsersync     = require('browser-sync').create(),
+    gulp            = require('gulp'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    cleancss        = require('gulp-clean-css'),
+    concat          = require('gulp-concat'),
+    csscomb         = require('gulp-csscomb'),
+    imagemin        = require('gulp-imagemin'),
+    csslint         = require('gulp-csslint'),
+    jshint          = require('gulp-jshint'),
+    rename          = require('gulp-rename'),
+    sass            = require('gulp-sass'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    uglify          = require('gulp-uglify');
 
-var noop        = require("through2").obj();
+var noop            = require("through2").obj();
 
 /**
  *  Config
@@ -163,19 +163,19 @@ gulp.task('style', () => {
     var $ = plugins.style;
 
     return gulp.src(getExtensions(paths.src.style, paths.src.root))
-        .pipe($.run.sourcemaps && $.run.sass ? sourcemaps.init($.sourcemaps.options) : noop)
-        .pipe($.run.sass ? sass($.sass.options).on('error', $.sass.onError) : noop)
-        .pipe($.run.sass ? concat($.concat.name) : noop)
-        .pipe($.run.autoprefixer && $.run.sass ? autoprefixer($.autoprefixer.options) : noop)
-        .pipe($.run.csscomb && $.run.sass ? csscomb($.csscomb.options) : noop)
-        .pipe($.run.csslint && $.run.sass ? csslint($.csslint.options) : noop)
-        .pipe($.run.csslint && $.run.sass ? csslint.formatter($.csslint.formatterOptions) : noop)
-        .pipe($.run.sass ? gulp.dest(paths.dest.root + '/' + paths.dest.style.folder) : noop)
-        .pipe($.run.cleancss && $.run.sass ? rename({ suffix: '.min', basename: $.rename.basename }) : noop)
-        .pipe($.run.cleancss && $.run.sass ? cleancss($.cleancss.options, $.cleancss.callback) : noop)
-        .pipe($.run.sourcemaps && $.run.sass ? sourcemaps.write($.sourcemaps.writePath, $.sourcemaps.writeOptions) : noop)
+        .pipe($.run.sourcemaps && $.run.sass    ? sourcemaps.init($.sourcemaps.options)                                 : noop)
+        .pipe($.run.sass                        ? sass($.sass.options).on('error', $.sass.onError)                      : noop)
+        .pipe($.run.sass                        ? concat($.concat.name)                                                 : noop)
+        .pipe($.run.autoprefixer && $.run.sass  ? autoprefixer($.autoprefixer.options)                                  : noop)
+        .pipe($.run.csscomb && $.run.sass       ? csscomb($.csscomb.options)                                            : noop)
+        .pipe($.run.csslint && $.run.sass       ? csslint($.csslint.options)                                            : noop)
+        .pipe($.run.csslint && $.run.sass       ? csslint.formatter($.csslint.formatterOptions)                         : noop)
+        .pipe($.run.sass                        ? gulp.dest(paths.dest.root + '/' + paths.dest.style.folder)            : noop)
+        .pipe($.run.cleancss && $.run.sass      ? rename({ suffix: '.min', basename: $.rename.basename })               : noop)
+        .pipe($.run.cleancss && $.run.sass      ? cleancss($.cleancss.options, $.cleancss.callback)                     : noop)
+        .pipe($.run.sourcemaps && $.run.sass    ? sourcemaps.write($.sourcemaps.writePath, $.sourcemaps.writeOptions)   : noop)
         .pipe(gulp.dest(paths.dest.root + '/' + paths.dest.style.folder))
-        .pipe(plugins.browsersync ? browsersync.stream() : noop);
+        .pipe(plugins.browsersync               ? browsersync.stream()                                                  : noop);
 });
 
 /**
@@ -186,16 +186,16 @@ gulp.task('scripts', () => {
     var $ = plugins.scripts;
 
     return gulp.src(getExtensions(paths.src.scripts, paths.src.root))
-        .pipe($.run.jshint ? jshint() : noop)
-        .pipe($.run.jshint ? jshint.reporter($.jshint.reporter) : noop)
-        .pipe($.run.sourcemaps && $.run.uglify ? sourcemaps.init($.sourcemaps.options) : noop)
+        .pipe($.run.jshint                      ? jshint() : noop)
+        .pipe($.run.jshint                      ? jshint.reporter($.jshint.reporter)                                    : noop)
+        .pipe($.run.sourcemaps && $.run.uglify  ? sourcemaps.init($.sourcemaps.options)                                 : noop)
         .pipe(concat($.concat.name))
         .pipe(gulp.dest(paths.dest.root + '/' + paths.dest.scripts.folder))
-        .pipe($.run.uglify ? uglify() : noop)
-        .pipe($.run.uglify ? rename({ suffix: '.min', basename: $.rename.basename }) : noop)
-        .pipe($.run.sourcemaps && $.run.uglify ? sourcemaps.write($.sourcemaps.writePath, $.sourcemaps.writeOptions) : noop)
+        .pipe($.run.uglify                      ? uglify()                                                              : noop)
+        .pipe($.run.uglify                      ? rename({ suffix: '.min', basename: $.rename.basename })               : noop)
+        .pipe($.run.sourcemaps && $.run.uglify  ? sourcemaps.write($.sourcemaps.writePath, $.sourcemaps.writeOptions)   : noop)
         .pipe(gulp.dest(paths.dest.root + '/' + paths.dest.scripts.folder))
-        .pipe(plugins.browsersync ? browsersync.stream() : noop);
+        .pipe(plugins.browsersync               ? browsersync.stream()                                                  : noop);
 });
 
 /**
@@ -206,9 +206,9 @@ gulp.task('images', () => {
     var $ = plugins.images;
 
     return gulp.src(getExtensions(paths.src.images, paths.root))
-        .pipe($.run.imagemin ? imagemin($.imagemin.options) : noop)
+        .pipe($.run.imagemin                    ? imagemin($.imagemin.options)                                          : noop)
         .pipe(gulp.dest(paths.dest.root + '/' + paths.dest.images.folder))
-        .pipe(plugins.browsersync ? browsersync.stream() : noop);
+        .pipe(plugins.browsersync               ? browsersync.stream()                                                  : noop);
 });
 
 /**
@@ -220,7 +220,7 @@ gulp.task('fonts', () => {
 
     return gulp.src(getExtensions(paths.src.fonts, paths.src.root))
         .pipe(gulp.dest(gulp.dest.root + '/' + paths.dest.fonts.folder))
-        .pipe(plugins.browsersync ? browsersync.stream() : noop);
+        .pipe(plugins.browsersync               ? browsersync.stream()                                                  : noop);
 });
 
 /**
